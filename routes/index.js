a0a5d131310d71fbc375e92fb04976fbfa706358
Minsun91 <minsunkim167@gmail.com>
2022-08-09 08:05:04 +0200

    주석님 develop 받기 전 파일

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-
 const postRouter = require("./posts.routes");
 const commentRouter = require("./comments.routes");
 const userRouter = require("./users.routes");
-const likeRouter = require("./likes.routes");
-
+// const likeRouter = require("./likes.routes");
 
 router.use("/users", userRouter);
 router.use("/posts/", postRouter);
 router.use("/comments/", commentRouter);
 router.use("/", userRouter);
-
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,9 +1,5 @@
 const express = require("express");
 const auth = require("../middlewares/auth-middleware");
-<<<<<<< HEAD
-=======
-
->>>>>>> 42c1444ba64d6aa0390aeac54da6202e99418487
 const router = express.Router();
 
 const UsersController = require("../controllers/users.controller");
@@ -15,17 +11,13 @@ router.post("/signup", usersController.createUser);
 // 로그인
 router.post("/signin", usersController.signinUser);
 
-<<<<<<< HEAD
-// // 로그아웃
-=======
 // 로그아웃
->>>>>>> 42c1444ba64d6aa0390aeac54da6202e99418487
 router.get("/logout", usersController.logoutUser);
 
-// // 유저 수정
+// 유저 수정
 router.patch("/edit/:userId", usersController.updateUser);
 
-// // 회원 탈퇴
+// 회원 탈퇴
 router.delete("/delete/:userId", usersController.deleteUser);
 
 module.exports = router;
