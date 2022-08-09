2847b679cd0eab265137e47329fd71f1563e3ab4
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-09 21:23:55 +0900

    최종:

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -14,14 +14,8 @@ router.post("/signin", usersController.signinUser);
 // 로그아웃
 router.get("/logout", usersController.logoutUser);
 
-<<<<<<< HEAD
-// // 유저 수정
-//router.patch("/edit/:userId", usersController.updateUser);
-=======
 // 유저 수정
 router.patch("/edit/:userId", auth, usersController.updateUser);
->>>>>>> e9f0f01c6961dd3b8b434803bd7c25337b5c262e
-
 
 // 회원 탈퇴
 router.delete("/delete", auth, usersController.deleteUser);
