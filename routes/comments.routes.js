c878f14da1764107dc57d1c9d00e76b2a6ad7411
phenomenonlee <flejdud8@naver.com>
2022-08-09 15:01:02 +0900

    1

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middlewares/auth-middleware");
 
 const CommentsController = require("../controllers/comments.controller");
 const commentsController = new CommentsController();
 
 router.get("/:postId", commentsController.getComments);
-router.post("/:postId", commentsController.createComment);
-router.patch("/:postId/:commentId", commentsController.updateComment);
-router.delete("/:postId/:commentId", commentsController.deleteComment);
+router.post("/:postId", authMiddleware, commentsController.createComment);
+router.patch(
+  "/:postId/:commentId",
+  authMiddleware,
+  commentsController.updateComment
+);
+router.delete(
+  "/:postId/:commentId",
+  authMiddleware,
+  commentsController.deleteComment
+);
 
 module.exports = router;
