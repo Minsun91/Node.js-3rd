ce6ccea6692b535d5a6ded24d44f634666993303
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-09 23:05:13 +0900

    레알최종본클론

--- controller.test.js
+++
@@ -1,8 +0,0 @@
-const CommentRepository = require("./repositories/comments.repository");
-const commentRepository = new CommentRepository();
-
-describe("comment", () => {
-  test("Comment 조회", () => {
-    expect(commentRepository.findAllComment(1)).toEqual(true);
-  });
-});
